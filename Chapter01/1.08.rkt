ce6d8db907700fcf75dd0d7b394132b3f7694746
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-07-10 01:10:24 +0500

    add 1.08

---
+++ Chapter01/1.08.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require rackunit)
+
+(define (square x) (* x x))
+
+(define (cubert x)
+  (sqrt-iter x 1.0 0))
+
+(define (sqrt-iter x guess prev-guess)
+  (if (good-enough? prev-guess guess)
+      guess
+      (sqrt-iter x (improve x guess) guess)))
+
+(define (good-enough? prev-guess guess)
+  (< (abs (- guess prev-guess)) 0.00000001))
+
+(define (improve x guess)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+;TEST
+
+(check-equal? (cubert 27) 3.0)
+(check-equal? (cubert 8) 2.0)
