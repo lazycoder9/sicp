c1d82b69ee641f2081fe17805b8f601253502241
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-11-20 23:25:33 +0500

    refactor and add test 2.12

--- Chapter02/2.12.rkt
+++ Chapter02/2.12.rkt
@@ -1,36 +1,7 @@
 #lang racket
 
-(define (make-interval a b)
-  (cons a b))
-
-(define (lower-bound interval)
-  (car interval))
-
-(define (upper-bound interval)
-  (cdr interval))
-
-(define (add-interval x y)
-  (make-interval (+ (lower-bound x) (lower-bound y))
-                 (+ (upper-bound x) (upper-bound y))))
-
-(define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound y)))
-        (p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4)
-                   (max p1 p2 p3 p4))))
-
-(define (div-interval x y)
-  (if (or (= (upper-bound y) 0) (= (lower-bound y) 0))
-      (display "ERROR BLYA!")      
-      (mul-interval x
-                    (make-interval (/ 1.0 (upper-bound y))
-                                   (/ 1.0 (lower-bound y))))))
-
-(define (sub-interval x y) 
-  (make-interval (- (lower-bound x) (upper-bound y)) 
-                 (- (upper-bound x) (lower-bound y))))
+(require rackunit)
+(require "2.7-2.8.rkt")
 
 (define (make-cnter-width c w)
   (make-interval (- c w) (+ c w)))
@@ -48,4 +19,9 @@
 
 (define (percent i)
   (* (/ (width i) (center i)) 100))
-  
+
+;TEST
+(define i (make-center-percent 10 10))
+
+(check-eq? (lower-bound i) 9)
+(check-eq? (upper-bound i) 11)
