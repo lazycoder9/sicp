7a5598ef0e7a1bf1462a68759492b954b8c1041c
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-17 18:08:42 +0500

    add 2.23

---
+++ Chapter02/2.23.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(define (for-each proc items)
+  (cond ((not (null? items)))
+        ((proc (car items)) (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x)) (list 1 2 3))
