beefe88c8c1956e5ab6cd141996c5010dde78e4b
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-17 20:04:10 +0500

    refactor and add tests 2.28

--- Chapter02/2.28.rkt
+++ Chapter02/2.28.rkt
@@ -1,34 +1,5 @@
 #lang racket
-
-(define (list-ref items n)
-  (if (= n 0)
-      (car items)
-      (list-ref (cdr items) (- n 1))))
-
-(define (length items)
-  (if (null? items)
-      0
-      (+ 1 (length (cdr items)))))
-
-(define (append list1 list2)
-  (if (null? list1)
-      list2
-      (cons (car list1) (append (cdr list1) list2))))
-
-(define (last-pair list)
-  (if (= (length list) 1)
-      list
-      (last-pair (cdr list))))
-
-(define (reverse l)
-  (define (iter rev-list list)
-    (cond ((null? list)
-           rev-list)
-          ((pair? (car list))
-           (iter (cons (iter '() (car list)) rev-list) (cdr list)))
-          (else
-           (iter (cons (car list) rev-list) (cdr list)))))
-  (iter '() l))
+(require rackunit)
 
 (define (fringe l)
   (define (remove-list items acc)
@@ -39,15 +10,8 @@
           (else
            (remove-list (cdr items) (cons (car items) acc)))))
   (reverse (remove-list l '())))
-    
 
+;TEST
 (define x (list 1 2 (list 3 5) (list (list 4 3) 2)))
-
-
-
-
-
-
-
-
-  
+(define expected (list 1 2 3 5 4 3 2))
+(check-equal? (fringe x) expected)
