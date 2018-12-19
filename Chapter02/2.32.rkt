fb6d5bc7512cf7367cef885f7e8abdaa3a0fadf8
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-19 20:57:49 +0500

    add tests 2.32

--- Chapter02/2.32.rkt
+++ Chapter02/2.32.rkt
@@ -1,23 +1,13 @@
 #lang racket
+(require rackunit)
 
-(define (subsets s) 
-  (if (null? s) 
-      '() 
-      (let ((rest (subsets (cdr s)))) 
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (ss) (cons (car s) ss)) rest)))))
 
-(define x (list 1 2 3))
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+;TEST
+(define x '(1 2 3))
+(define expected '(() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3)))
+(check-equal? (subsets x) expected)
