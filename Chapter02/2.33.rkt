a8402589582278cf8dfdb9005371d29ae6ac02f3
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-20 00:19:40 +0500

    add tests to 2.33

--- Chapter02/2.33.rkt
+++ Chapter02/2.33.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require rackunit)
 
 (define (accumulate op init items)
   (if (null? items)
@@ -15,6 +16,10 @@
 (define (length sequence)
   (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
 
+;TEST
 (define x (list 1 2 3 4))
-
 (define y (list 5 6 7))
+
+(check-equal? (map (lambda (x) (* x 2)) x) '(2 4 6 8))
+(check-equal? (append x y) '(1 2 3 4 5 6 7))
+(check-equal? (length x) 4)
