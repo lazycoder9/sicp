ae54ddad76152825ae7a7d652d15b011dee222e5
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-20 15:23:08 +0500

    add tests 2.36

--- Chapter02/2.36.rkt
+++ Chapter02/2.36.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require rackunit)
 
 (define (accumulate op init items)
   (if (null? items)
@@ -12,4 +13,6 @@
       (cons (accumulate op init (map (lambda (x) (car x)) seqs))
             (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
 
+;TEST
 (define x (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(check-equal? (accumulate-n + 0 x) (list 22 26 30))
